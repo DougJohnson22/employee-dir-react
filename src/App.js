e43afe94e9922932d1fd22f5eee96a183b474e54
Douglas Johnson <trunks22a@gmail.com>
2021-02-16 18:11:17 -0800

    react router set-up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./pages/Landing"
+import NavBar from "./components/NavBar"
 import './App.css';
 
 function App() {
   return (
-<Router>
-    <div>
-
-
-
-    </div>
-</Router>
+    <Router>
+      <div>
+        <NavBar />
+        <Route exact path="/">
+          <Landing />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
